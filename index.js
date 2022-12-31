4f4b8b52a39d052146d5cb1870f6cd8025ed01cd
Edwin Moreno <edwinmoreno77@gmail.com>
2022-12-31 15:13:39 -0400

    vercel deployment

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 const express = require('express');
-const cors = require('cors');
-const { dbConnection } = require('./database/config');
+const app = express();
+
 const path = require('path');
 const logger = require("morgan");
-
+const cors = require('cors');
 require('dotenv').config();
+
+const { dbConnection } = require('./database/config');
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 
 const PORT = process.env.PORT || 4000;
 
-const app = express();
+// const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 
 //database
 dbConnection();
 
 //CORS
-app.use(cors())
+// app.use(cors())
 
 //public folder         //this is for the react app
 // app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, "./public")));
 
 
 //read and parse body
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 
 //routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 //redirect to index.html   //this is for the react app
 
