bce30d942d47223ec4491aae63b519e7e6e5c8ce
Edwin Moreno <edwinmoreno77@gmail.com>
2022-12-31 16:57:20 -0400

    vercel

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
+const { dbConnection } = require('./database/config');
 
 const path = require('path');
 const logger = require("morgan");
+
+//CORS
 const cors = require('cors');
+//DOTENV
 require('dotenv').config();
 
-const { dbConnection } = require('./database/config');
 
 app.use(logger("dev"));
+
+
+//read and parse body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
@@ -20,34 +26,21 @@ const PORT = process.env.PORT || 4000;
 
 // app.use(logger("dev"));
 
-
 //database
 dbConnection();
-
-//CORS
-// app.use(cors())
-
-//public folder         //this is for the react app
-// app.use(express.static('public'));
-
-
-//read and parse body
-// app.use(express.json());
-
 
 //routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+//to deploy react app whith nodejs in heroku
+// app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-
-//redirect to index.html   //this is for the react app
-
+//to deploy react app whith nodejs in heroku
 // app.get('*', (req, res) => {
 //     res.sendFile(__dirname + '/public/index.html');
 // });
-
 app.get("*", function (_, res) {
     res.sendFile(
         path.join(__dirname, "./frontend/build/index.html"),
@@ -58,8 +51,6 @@ app.get("*", function (_, res) {
         }
     );
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
