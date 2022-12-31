4aa8cc6b9ba59e0f98bc6d1ca7709e22a696dcec
Edwin Moreno <edwinmoreno77@gmail.com>
2022-12-31 14:36:36 -0400

    vercel deployment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path');
+const logger = require("morgan");
+
 require('dotenv').config();
 
 
@@ -8,27 +11,45 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(logger("dev"));
+
+
 //database
 dbConnection();
 
 //CORS
 app.use(cors())
 
-//public folder
+//public folder         //this is for the react app
 // app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "./public")));
+
 
 //read and parse body
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 //routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
-//redirect to index.html
+//redirect to index.html   //this is for the react app
 
 // app.get('*', (req, res) => {
 //     res.sendFile(__dirname + '/public/index.html');
 // });
+
+app.get("*", function (_, res) {
+    res.sendFile(
+        path.join(__dirname, "./public/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+});
 
 
 
