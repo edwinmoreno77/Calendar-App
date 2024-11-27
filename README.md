7e3cd4897f79ab4529eaac3d87c706519a6a0368
Edwin Moreno <edwinmoreno77@gmail.com>
2024-11-27 18:36:51 -0300

    gifs and README.md added

---
+++ README.md
@@ -0,0 +1,14 @@
+run `npm install ` to rebuild node modules.
+
+
+<div style="display: flex; justify-content: center; gap: 10px;">
+  <img src="https://github.com/edwinmoreno77/calendar-backend/tree/main/assets/calendar.gif" alt="calendar" width="300">
+  <img src="https://github.com/edwinmoreno77/calendar-backend/tree/main/assets/calendarEvent.gif" alt="events" width="300">
+</div>
+
+
+## Deployed in
+
+```
+https://calendar-backend-38sm.onrender.com
+```
--- index.js
+++ index.js
@@ -19,15 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
-// Configuración de CORS
-// const corsOptions = {
-//     origin: process.env.ALLOWED_ORIGIN || '*', // Cambia "*" a un origen específico en producción
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// };
-// app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions)); // Manejo de preflight
-
 //database
 dbConnection();
 
@@ -35,14 +26,8 @@ dbConnection();
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
-//to deploy react app whith nodejs in heroku
-// app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-//to deploy react app whith nodejs in heroku
-// app.get('*', (req, res) => {
-//     res.sendFile(__dirname + '/public/index.html');
-// });
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
